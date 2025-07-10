20daabc181ac9f6f41cf225233fe29607ff5afbf
RUDRA K CHAUHAN <160327942+rudrachauhn007@users.noreply.github.com>
2025-07-10 22:28:58 +0530

    /rudra

---
+++ DAY-2/MAIN.cpp
@@ -0,0 +1,39 @@
+// shree ganeshaya namaha:|
+// shree chamudamataya namaha:|
+// shree saraswatimataya namaha:|
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            // Left half is sorted
+            if (nums[mid] >= nums[low]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1; // target not found
+    }
+};
+// link to question = https://leetcode.com/problems/search-in-rotated-sorted-array/
