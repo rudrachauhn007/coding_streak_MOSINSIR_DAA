716717d73c080656f7020e2780db0317e73c2677
RUDRA K CHAUHAN <160327942+rudrachauhn007@users.noreply.github.com>
2025-08-16 21:13:38 +0530

    Time: 75 ms (31.78%) | Memory: 82.5 MB (36.37%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,27 +1,31 @@
 // shree ganeshaya namaha:|
 // shree chamudamataya namaha:|
 // shree saraswatimataya namaha:|
+
 class Solution {
 public:
-    vector<vector<int>> ans;
-
-    void backtrack(int start, int n, int k, vector<int>& curr) {
-        // if combination is ready
-        if (curr.size() == k) {
-            ans.push_back(curr);
-            return;
+vector<vector<int>> ans;
+        void backtrack(int start,int n,int k,vector<int> &cur)
+        {
+            if (cur.size()==k)
+            {
+                ans.push_back(cur);
+                return;
+            }
+            for (int i=start;i<=n;i++)
+        {
+            cur.push_back(i);
+            backtrack(i+1,n,k,cur);
+            cur.pop_back();
         }
-        // try all possible choices
-        for (int i = start; i <= n; i++) {
-            curr.push_back(i);
-            backtrack(i + 1, n, k, curr);
-            curr.pop_back(); // undo choice
         }
-    }
+        
 
     vector<vector<int>> combine(int n, int k) {
-        vector<int> curr;
-        backtrack(1, n, k, curr);
+        vector<int> cur;
+        backtrack(1,n,k,cur);
         return ans;
+        
+        
     }
 };
