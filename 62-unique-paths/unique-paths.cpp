24082d269d397a9c34d36d884a08e4858806804b
RUDRA K CHAUHAN <160327942+rudrachauhn007@users.noreply.github.com>
2025-09-07 19:21:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (33.89%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -3,28 +3,31 @@
 // shree saraswatimataya namaha:|
 class Solution {
 public:
-bool safe (int row,int col,int m,int n)
-{
-    return (row >=0 &&row <m && col>=0 && col <n);
-}
-int solve (int row,int col,int m,int n,vector<vector<int>>& dp)
-{
-     if (!safe(row,col,m,n)) return 0;
-
-     if (row == m-1 && col==n-1) return 1;
-
-     if (dp[row][col]!=-1) return dp[row][col];
-
-     int right = solve(row,col+1,m,n,dp);
-     int down= solve(row+1,col,m,n,dp);
-
-     return dp[row][col]= right+down;
-}
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        vector<vector<int>> dp(m,vector<int> (n,0));
+        dp[0][0]=1;
+        for (int i=0;i<m;i++)
+        {
+            for (int j=0;j<n;j++)
+            {
+                if (i==0 && j==0) continue; 
 
-        int ans= solve(0,0,m,n,dp);
-        return ans;
+                int up=dp[i][j];
+                if (i-1>=0)
+                {
+                    up += dp[i-1][j];
+                }
+                int left= dp[i][j];
+
+                if (j-1>=0)
+                {
+                    left += dp[i][j-1];
+                }
+
+                dp[i][j]=up+left;
+            }
+        }
+        return dp[m-1][n-1];
         
     }
 };
