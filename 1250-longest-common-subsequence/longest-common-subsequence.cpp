fc1c436c6b9e505ee5094f2a64258481bc722d29
RUDRA K CHAUHAN <160327942+rudrachauhn007@users.noreply.github.com>
2025-09-13 20:49:57 +0530

    Time: 26 ms (64.50%) | Memory: 27.5 MB (52.59%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -19,9 +19,26 @@ int recc (int i,int j,string& s1,string& s2,vector<vector<int>>&dp)
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for (int i=0;i<=m;i++) dp[0][i]=0;
+        for (int j=0;j<=n;j++) dp[j][0]=0;
 
-        return recc(n-1,m-1,text1,text2,dp);
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=m;j++)
+            {
+                if (text1[i-1]==text2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+
         
     }
 };
