80e63599748af9f02d254ff5c891365369926f79
RUDRA K CHAUHAN <160327942+rudrachauhn007@users.noreply.github.com>
2025-12-10 07:05:12 +0530

    Time: 4 ms (63.04%) | Memory: 13.3 MB (96.69%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -0,0 +1,15 @@
+// shree ganeshaya namaha:|
+// shree chamudamataya namaha:|
+// shree saraswatimataya namaha:|
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans;
+        for (int i=0;i<n;i++)
+        {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[i+n]);
+        }
+        return ans;
+    }
+};
