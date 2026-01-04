e4b8c530d9b441b1c792e6c2d2c626d68d1c22f0
RUDRA K CHAUHAN <160327942+rudrachauhn007@users.noreply.github.com>
2026-01-04 22:09:28 +0530

    Time: 79 ms (26.60%) | Memory: 38.1 MB (7.39%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.cpp
@@ -0,0 +1,37 @@
+// shree ganeshaya namaha:|
+// shree chamudamataya namaha:|
+// shree saraswatimataya namaha:|
+class Solution {
+public:
+    int sum (int n)
+    {
+        vector<int> ans;
+        int cnt=0;
+        for (int i=1;i*i<=n;i++)
+        {
+             if (n%i==0)
+             {
+                ans.push_back(i);
+                if (i != (n/i))
+                {
+                    ans.push_back(n/i);
+                }
+             }
+        }
+        if (ans.size()==4)
+        {
+            for (auto it : ans) cnt += it;
+        }
+
+        return cnt;
+    }
+    int sumFourDivisors(vector<int>& nums) {
+        int ans=0;
+        for (auto it : nums)
+        {
+            ans += sum(it);
+        }
+        return ans;
+        
+    }
+};
