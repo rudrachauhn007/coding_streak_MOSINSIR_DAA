a083805ffec6ffe6cc968d6ce759d6119ee55faf
RUDRA K CHAUHAN <160327942+rudrachauhn007@users.noreply.github.com>
2026-01-10 21:26:34 +0530

    Time: 40 ms (26.67%) | Memory: 64.7 MB (16.71%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -0,0 +1,23 @@
+// shree ganeshaya namaha:|
+// shree chamudamataya namaha:|
+// shree saraswatimataya namaha:|
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+         priority_queue<int> pq;
+         for (auto it : nums)
+         pq.push(it);
+      
+         while (k>1)
+         {
+            pq.pop();
+            k--;
+         }
+         return pq.top();
+
+       
+       
+        return 0;
+        
+    }
+};
