488e130348202e9bb0b39d6d53a234990deffa7f
RUDRA K CHAUHAN <160327942+rudrachauhn007@users.noreply.github.com>
2026-05-14 11:32:48 +0530

    Time: 3 ms (20.41%) | Memory: 27.4 MB (14.99%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -0,0 +1,32 @@
+// shree ganeshaya namaha:|
+// shree chamudamataya namaha;|
+// shree saraswatimataya namaha:|
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n=nums.size()-1;
+        int ans = ((n)*(n+1))/2  + n ;
+        int cnt=0;
+        map<int,int>mpp;
+        int maxi=0;
+        for (auto it : nums)
+        {
+            mpp[it]++;
+            cnt += it;
+            maxi = max(it,maxi);
+        }
+
+        if (mpp.size() == n && n == maxi)
+        {
+            if (ans == cnt)
+            {
+                return true;
+            }
+        }
+
+        //cout <<mpp.size() << " " << cnt << " " << ans << endl;
+
+
+        return false;
+    }
+};
