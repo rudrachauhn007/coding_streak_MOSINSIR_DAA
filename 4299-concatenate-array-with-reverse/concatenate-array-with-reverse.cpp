f822ff236c68f1bac6d1bdf8e629362386cdf30d
RUDRA K CHAUHAN <160327942+rudrachauhn007@users.noreply.github.com>
2026-05-22 15:39:58 +0530

    Time: 0 ms (100.00%) | Memory: 30.8 MB (42.06%) - LeetSync

---
+++ 4299-concatenate-array-with-reverse/concatenate-array-with-reverse.cpp
@@ -0,0 +1,15 @@
+// shree ganeshaya namaha:|
+// shree chamudamataya namaha:|
+// shree saraswatimataya namaha:|
+class Solution {
+public:
+    vector<int> concatWithReverse(vector<int>& nums) {
+        int n=nums.size()-1;
+        for (int i=n ;i>=0;i--)
+        {
+            nums.push_back(nums[i]);
+        }
+        return nums;
+        
+    }
+};
